ae8aca5b2a7b1dcd3108b9190e07afd49d635c07
TheHiddenCoder001 <thehiddencoder001@gmail.com>
2026-02-06 12:14:35 +0530

    Added wintoast notifications

--- RobloxBadgeDumper/robloxbadgedumper.py
+++ RobloxBadgeDumper/robloxbadgedumper.py
@@ -5,12 +5,22 @@ import sqlite3
 import json
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from winotify import *
 badges = []
+cursor = None
+
+def notification(directory,badges):
+    toast = Notification(app_id="Roblox Badge Dumper",title="Roblox Badge Dumper",msg= f"Badge Dump Complete!\nBadges Dumped: {badges}",icon=r"c:\path\to\icon.png")
+    toast.set_audio(audio.Reminder, loop=False)
+    toast.add_actions(label="Open Folder",launch=str(directory))
+    toast.add_actions(label="Open Github",launch=(str("https://github.com/TheHiddenCoder001/Python-Projects/tree/main/RobloxBadgeDumper")))
+    toast.show()
+
 #------------------------------------------------------------
 def parse():
     parser = argparse.ArgumentParser(description="CLI tool for checking a users badges. Output in .txt file")
     parser.add_argument("username", type=str,help="Username to check")
-    parser.add_argument("output",type=int,help="Way to output (0=txt file, 1=db file),",nargs='?',default=1)
+    parser.add_argument("output",type=int,help="Way to output (0=txt file, 1=db file),",nargs='?',default=0)
     return parser.parse_args()
 parser = parse()
 #------------------------------------------------------------
@@ -29,7 +39,6 @@ session.mount('https://', adapter)
 session.mount("http://", adapter)
 username = parser.username
 output = parser.output
-cursor = None
 request = session.post("https://users.roblox.com/v1/usernames/users", json={"usernames": [username], "excludeBannedUsers": False},timeout=15)
 data = request.json()
 if not data['data']:
@@ -59,6 +68,8 @@ if parser.output == 0:
             link = f"https://www.roblox.com/badges/{badge['id']}/"
             json.dump(badge, f, ensure_ascii=False)
     print(f"Text file updated with {len(badges)} badges.")
+    print(f"File located at {user_directory}")
+    notification(user_directory,len(badges))
 else:
     database_directory = os.path.join(user_directory, "badges.db")
     conn = sqlite3.connect(database_directory)
@@ -71,8 +82,13 @@ else:
         cursor_db.execute("INSERT OR IGNORE INTO badges (name, website_link, id) VALUES (?, ?, ?)", (badge['name'], link, badge['id']))
     conn.commit()
     cursor_db.execute("SELECT COUNT(*) FROM badges")
-    print(f"Database updated. Total badges in DB: {cursor_db.fetchone()[0]}")
+    total_badges = cursor_db.fetchone()[0]
+    
+    print(f"Database updated. Total badges in DB: {total_badges}")
+    print(f"File located at {user_directory}")
+    notification(user_directory,total_badges)
     conn.close()
+
 #------------------------------------------------------------
 
 
